352a86c34725970fdf55ea7a41796407ed1a3beb
Søren Mortensen <soren@neros.dev>
2020-04-15 16:49:41 +0100

    Set up deployment through Heroku (#2)
    * Add heroku.yml
    
    * Switch to nightly rust
    
    * Add dependency on Rocket
    
    * Add a hello world and healthcheck endpoint
    
    * Clean up healthcheck() definition
    
    * Configure zical to take a PORT at runtime
    
    * Disable troublesome clippy warnings

--- zical/src/main.rs
+++ zical/src/main.rs
@@ -9,8 +9,24 @@
 //  http://opensource.org/licenses/MIT>, at your option. This file may not be
 //  copied, modified, or distributed except according to those terms.
 
-use zical::print_hello;
+#![feature(proc_macro_hygiene, decl_macro)]
+#![allow(clippy::unit_arg, clippy::let_unit_value)]
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+/// Healthcheck endpoint, always returns `200 OK` with no content when app is running.
+#[get("/healthcheck")]
+fn healthcheck() {
+}
 
 fn main() {
-    print_hello();
+    rocket::ignite()
+        .mount("/", routes![index, healthcheck])
+        .launch();
 }

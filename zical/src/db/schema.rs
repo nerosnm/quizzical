1606efdb4b2c25a4e911b2d4a9e6ef9d629d0efe
Søren Mortensen <soren@neros.dev>
2020-04-15 17:32:58 +0100

    Set up Diesel schema file patch

--- zical/src/db/schema.rs
+++ zical/src/db/schema.rs
@@ -1,3 +1,14 @@
+//  db/schema.rs
+//  zical
+//
+//  Created by Diesel CLI <diesel-cli@example.com> on 2020-04-15.
+//  Copyright (c) 2020 Søren Mortensen.
+//
+//  Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+//  http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+//  http://opensource.org/licenses/MIT>, at your option. This file may not be
+//  copied, modified, or distributed except according to those terms.
+
 table! {
     teams (id) {
         id -> Int4,
